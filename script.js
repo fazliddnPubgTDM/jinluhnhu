3b0f51af86a6248c93b17e2907ab842e36942892
fazliddnPubgTDM <fazliddine214@gmail.com>
2025-03-13 10:31:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// console.log('muzika')
+// alert('script,js')
+
+
+let age = 25;
+age = 30;
+console.log(age);
+
+const pi = 3.14;
+
+
+let name1 = prompt('ismingizni kiriting')
+console.log(name1)
+
+let name2 = prompt('familiyani kiriting')
+console.log(name2)
+
+let name3 = prompt('yoshingizni kiriting')
+console.log(name3)
+
+let name4 = prompt('yoqtirgan fanizni kiriting')
+console.log(name4)
+
+let name5 = prompt('yashash joyizni kiriting')
+console.log("manzil", name5)
+
+let thanks = alert('malumot uchun rahmat')
